203eab0884036be513bbcaf5d314d5f28788da53
Guravani <guravani@example.com>
2026-01-01 21:23:45 +0530

    Initial commit for saas-project

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("SaaS backend is running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
